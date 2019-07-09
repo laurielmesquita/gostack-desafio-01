958bb4ce2b4923abeefbc56c75cd9e7db2fa15bf
Lauriel Mesquita <laurielmesquita@me.com>
2019-07-09 01:38:24 -0300

    Added task title

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ server.post('/projects', (req, res) => {
   return res.json(project)
 })
 
-server.post('/projects/:id/tasks', (req, res) => {
-  const { tasks } = req.body.tasks
-
-  projects.push(tasks)
-
-  return res.json(projects.tasks)
-})
-
 server.get('/projects', (req, res) => {
   return res.json(projects)
 })
@@ -46,6 +38,17 @@ server.put('/projects/:id', (req, res) => {
   const project = projects.find(p => p.id === id)
 
   project.title = title
+
+  return res.json(project)
+})
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  const project = projects.find(p => p.id === id)
+
+  project.tasks.push(title)
 
   return res.json(project)
 })
