2633e77fb8019d46e612ac765aa3ba34ca937631
Lauriel Mesquita <laurielmesquita@me.com>
2019-07-09 01:08:21 -0300

    Added list all projects and update projects

--- index.js
+++ index.js
@@ -3,4 +3,43 @@ const server = express()
 
 server.use(express.json())
 
+const projects = []
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+
+  projects.push(project)
+
+  return res.json(project)
+})
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { tasks } = req.body.tasks
+
+  projects.push(tasks)
+
+  return res.json(projects.tasks)
+})
+
+server.get('/projects', (req, res) => {
+  return res.json(projects)
+})
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  const project = projects.find(p => p.id === id)
+
+  project.title = title
+
+  return res.json(project)
+})
+
 server.listen(3000)
